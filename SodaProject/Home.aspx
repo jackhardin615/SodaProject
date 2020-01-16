70dcdec97f8ade75a477875919896f5fc21453b7
Jack Hardin <48068098+jackhardin@users.noreply.github.com>
2020-01-16 17:59:39 -0600

    Add project files.

---
+++ SodaProject/Home.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SodaProject.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home</title>
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/Site.css" rel="stylesheet" />
+    </head>
+<body>
+    <form id="form1" runat="server">
+        <div class="text-center">
+            <h1>Soda Machine App</h1></div>
+        <div class="text-center">
+        <asp:Button ID="insertCoinBtn" class="btn-default btn-primary btn-round" runat="server" Text="Insert Quarter" Font-Size="X-Large" Height="43px" Width="179px" OnClick="insertCoinBtn_Click" />
+        <asp:Button ID="refundBtn" class="btn-default btn-success btn-round" runat="server" Text="Refund Quarter" Font-Size="X-Large" Height="43px" Width="186px" Visible="False" OnClick="refundBtn_Click" />
+           <br />
+            <asp:Label ID="totallbl" runat="server" Text="Total: " Font-Size="X-Large"></asp:Label>
+            <asp:Label ID="totalAmountlbl" runat="server" Text="$0.00" Font-Size="X-Large"></asp:Label>
+           <br />
+
+            <div class="table-responsive">
+            <asp:Table ID="sodaTable" CssClass="table table-bordered" runat="server">
+                <asp:TableRow runat="server" HorizontalAlign="Center" VerticalAlign="Middle">
+                    <asp:TableCell runat="server">
+                        <asp:Button ID="cokeBtn" runat="server" Text="Select" OnClick="cokeBtn_Click"/>
+</asp:TableCell>
+                    <asp:TableCell runat="server">Coca-Cola</asp:TableCell>
+                    <asp:TableCell runat="server">Available: <asp:Label ID="cokeAmountlbl" runat="server" Text=""></asp:Label>
+</asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server" HorizontalAlign="Center" VerticalAlign="Middle">
+                    <asp:TableCell runat="server">
+                        <asp:Button ID="dietCokeBtn" runat="server" Text="Select" OnClick="dietcokeBtn_Click" />
+</asp:TableCell>
+                    <asp:TableCell runat="server">Diet Coca-Cola</asp:TableCell>
+                    <asp:TableCell runat="server">Available: <asp:Label ID="dietcokeAmountlbl" runat="server" Text=""></asp:Label>
+</asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server" HorizontalAlign="Center" VerticalAlign="Middle">
+                    <asp:TableCell runat="server">
+                        <asp:Button ID="spriteBtn" runat="server" Text="Select" OnClick="spriteBtn_Click" />
+</asp:TableCell>
+                    <asp:TableCell runat="server">Sprite</asp:TableCell>
+                    <asp:TableCell runat="server">Available: <asp:Label ID="spriteAmountlbl" runat="server" Text=""></asp:Label>
+</asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server" HorizontalAlign="Center" VerticalAlign="Middle">
+                    <asp:TableCell runat="server">
+                        <asp:Button ID="drpepperBtn" runat="server" Text="Select" OnClick="drpepperBtn_Click" />
+</asp:TableCell>
+                    <asp:TableCell runat="server">Dr. Pepper</asp:TableCell>
+                    <asp:TableCell runat="server">Available: <asp:Label ID="drpepperAmountlbl" runat="server" Text=""></asp:Label>
+</asp:TableCell>
+                </asp:TableRow>
+
+            </asp:Table>
+               </div>
+
+            </div>
+
+    </form>
+</body>
+</html>
---
+++ SodaProject/Home.aspx.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+namespace SodaProject
+{
+
+    public partial class Home : System.Web.UI.Page
+    {
+        static double quarter = 0.00;
+        static string coinTotal = String.Format("{0:C}", quarter);
+        static int cokeAmount = 2;
+        static int dietcokeAmount = 5;
+        static int spriteAmount = 3;
+        static int drpepperAmount = 0;
+
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+
+            //Check if Coca-Cola is sold out
+            SoldOut(cokeAmountlbl, cokeAmount);
+
+            //Check if Diet Coca-Cola is sold out
+            SoldOut(dietcokeAmountlbl, dietcokeAmount);
+
+            //Check if Sprite is sold out
+            SoldOut(spriteAmountlbl, spriteAmount);
+
+            //Check if Dr. Pepper is sold out
+            SoldOut(drpepperAmountlbl, drpepperAmount);
+        }
+
+
+        //Insert Coin button
+        protected void insertCoinBtn_Click(object sender, EventArgs e)
+        {
+            refundBtn.Visible = true;
+            quarter += 0.25;
+            UpdateCoins(totalAmountlbl);
+
+        }
+
+
+        //Refund coin button
+        protected void refundBtn_Click(object sender, EventArgs e)
+        {
+
+            if (quarter > 0.25)
+            {
+                quarter -= 0.25;
+                UpdateCoins(totalAmountlbl);
+            }
+            else if (quarter == 0.25)
+            {
+                quarter -= 0.25;
+                UpdateCoins(totalAmountlbl);
+                refundBtn.Visible = false;
+            }
+            else
+            {
+                totalAmountlbl.Text = coinTotal;
+                refundBtn.Visible = false;
+            }
+
+        }
+
+
+        //Change available lable to sold out
+        static void SoldOut(Label label, int soda)
+        {
+            if (soda > 0)
+            {
+                label.Text = soda.ToString();
+            }
+            else
+            {
+                label.Text = "SOLD OUT";
+            }
+        }
+
+
+        //Update total number of coins in machine
+        static void UpdateCoins(Label label)
+        {
+            string coinTotal = String.Format("{0:C}", quarter);
+            label.Text = coinTotal;
+        }
+
+
+        //Coca-Cola button
+        protected void cokeBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (quarter > 0.25)
+            {
+                if (cokeAmount > 0)
+                {
+                    cokeAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(cokeAmountlbl, cokeAmount);
+                    UpdateCoins(totalAmountlbl);
+                }
+                else
+                {
+                    SoldOut(cokeAmountlbl, cokeAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                }
+                
+            }
+            else if (quarter == 0.25)
+            {
+                if (cokeAmount > 0)
+                {
+                    cokeAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(cokeAmountlbl, cokeAmount);
+                    UpdateCoins(totalAmountlbl);
+                }
+                else
+                {
+                    SoldOut(cokeAmountlbl, cokeAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                    refundBtn.Visible = false;
+                }
+                
+            }
+
+            else
+            {
+                refundBtn.Visible = false;
+                Response.Write("<script>alert('Insert a quarter');</script>");
+            }
+
+        }
+
+        //Diet Coke button
+        protected void dietcokeBtn_Click(object sender, EventArgs e)
+        {
+            if (quarter > 0.25)
+            {
+                if (dietcokeAmount > 0)
+                {
+                    dietcokeAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(dietcokeAmountlbl, dietcokeAmount);
+                    UpdateCoins(totalAmountlbl);
+                }
+                else
+                {
+                    SoldOut(dietcokeAmountlbl, dietcokeAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                }
+
+            }
+            else if (quarter == 0.25)
+            {
+                if (dietcokeAmount > 0)
+                {
+                    dietcokeAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(dietcokeAmountlbl, dietcokeAmount);
+                    UpdateCoins(totalAmountlbl);
+                }
+                else
+                {
+                    SoldOut(dietcokeAmountlbl, dietcokeAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                    refundBtn.Visible = false;
+                }
+
+            }
+
+            else
+            {
+                refundBtn.Visible = false;
+                Response.Write("<script>alert('Insert a quarter');</script>");
+            }
+
+
+        }
+
+
+
+
+        //Sprite button
+        protected void spriteBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (quarter > 0.25)
+            {
+                if (spriteAmount > 0)
+                {
+                    spriteAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(spriteAmountlbl, spriteAmount);
+                    UpdateCoins(totalAmountlbl);
+                }
+                else
+                {
+                    SoldOut(spriteAmountlbl, spriteAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                }
+
+            }
+            else if (quarter == 0.25)
+            {
+                if (spriteAmount > 0)
+                {
+                    spriteAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(spriteAmountlbl, spriteAmount);
+                    UpdateCoins(totalAmountlbl);
+                }
+                else
+                {
+                    SoldOut(spriteAmountlbl, spriteAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                    refundBtn.Visible = false;
+                }
+
+            }
+
+            else
+            {
+                refundBtn.Visible = false;
+                Response.Write("<script>alert('Insert a quarter');</script>");
+            }
+        }
+
+
+        //Dr. Pepper button
+        protected void drpepperBtn_Click(object sender, EventArgs e)
+        {
+            
+
+                if (quarter > 0.25)
+                {
+                    if (drpepperAmount > 0)
+                    {
+                        drpepperAmount -= 1;
+                        quarter -= 0.25;
+                        SoldOut(drpepperAmountlbl, drpepperAmount);
+                        UpdateCoins(totalAmountlbl);
+                    }
+                    else
+                    {
+                        SoldOut(drpepperAmountlbl, drpepperAmount);
+                        Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                    }
+
+                }
+                else if (quarter == 0.25)
+                {
+                    if (drpepperAmount > 0)
+                    {
+                    drpepperAmount -= 1;
+                    quarter -= 0.25;
+                    SoldOut(drpepperAmountlbl, drpepperAmount);
+                    UpdateCoins(totalAmountlbl);
+                    }
+                    else
+                    {
+                    SoldOut(drpepperAmountlbl, drpepperAmount);
+                    Response.Write("<script>alert('Sold Out. Please try another soda.');</script>");
+                    refundBtn.Visible = false;
+                    }
+
+                }
+
+                else
+                {
+                    refundBtn.Visible = false;
+                    Response.Write("<script>alert('Insert a quarter');</script>");
+                }
+
+            
+        }
+    }
+
+    
+    }
+
+
